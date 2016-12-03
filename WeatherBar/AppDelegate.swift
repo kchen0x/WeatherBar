396371fa3b2ab856ba6c819e707fb612b753a0cd
Kun Chen <quentin.chen@foxmail.com>
2016-12-02 18:47:18 -0800

    reconsctruct code with Controller

--- WeatherBar/AppDelegate.swift
+++ WeatherBar/AppDelegate.swift
@@ -10,29 +10,12 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var statusMenu: NSMenu!
-    
-    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
-
-    @IBAction func quickClicked(_ sender: NSMenuItem) {
-        NSApplication.shared().terminate(self)
-    }
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        statusItem.title = "WeatherBar"
-        statusItem.menu = statusMenu
-        
-        let icon = NSImage(named: "statusIcon")
-        icon?.isTemplate = true // best for dark mode
-        statusItem.image = icon
-        statusItem.menu = statusMenu
+        // Insert code here to tear down your application
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
---
+++ WeatherBar/StatusMenuController.swift
@@ -0,0 +1,26 @@
+//
+//  StatusMenuController.swift
+//  WeatherBar
+//
+//  Created by 陈坤 on 2016/12/2.
+//  Copyright © 2016年 Kun Chen. All rights reserved.
+//
+
+import Cocoa
+
+class StatusMenuController: NSObject {
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    
+    override func awakeFromNib() {
+        let icon = NSImage(named: "statusIcon")
+        icon?.isTemplate = true // best for dark mode
+        statusItem.image = icon
+        statusItem.menu = statusMenu
+    }
+    
+    @IBAction func quickClicked(_ sender: Any) {
+        NSApplication.shared().terminate(self)
+    }
+}
