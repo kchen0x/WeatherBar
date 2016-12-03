70012004a4451e601b115920851ca2e2fff38b8a
Kun Chen <quentin.chen@foxmail.com>
2016-12-02 22:28:29 -0800

    add weather icon and preferences

---
+++ WeatherBar/PreferencesWindow.swift
@@ -0,0 +1,19 @@
+//
+//  PreferencesWindow.swift
+//  WeatherBar
+//
+//  Created by 陈坤 on 2016/12/2.
+//  Copyright © 2016年 Kun Chen. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesWindow: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
