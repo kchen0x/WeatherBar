097dadae9198e7feb769f4d65b7a6e40ec17c3c5
Kun Chen <quentin.chen@foxmail.com>
2016-12-02 17:54:22 -0800

    Initial Commit

---
+++ WeatherBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WeatherBar
+//
+//  Created by 陈坤 on 2016/12/2.
+//  Copyright © 2016年 Kun Chen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
